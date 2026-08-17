84d364f42345847dbf0023cd6afdb3630748a7d0
Debangan <debangandey19@gmail.com>
2026-08-17 19:44:45 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,13 +22,22 @@ def save_object(file_path , obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train,y_train)         # Train the model
+            para = params[list(models.keys())[i]]
+
+            # Training the model after Hyper parameter tuning
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            # model.fit(x_train,y_train)         # Train the model
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
 
@@ -35,7 +45,8 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
             test_model_score = r2_score(y_test , y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
-            return report
+
+        return report
 
     except Exception as e:
         raise CustomException(e,sys)
